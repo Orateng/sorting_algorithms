5d6860b5bed4c959c74ba6ca4a5c857bcdb9d4aa
Orateng <1818.blackmail@gmail.com>
2023-07-30 02:09:29 +0000

    My filke

--- sort.h
+++ sort.h
@@ -47,6 +47,11 @@ void bitonic_sort(int *array, size_t size);
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
 		char flow);
+void quick_sort_hoare(int *array, size_t size);
+void hoare_sort(int *array, size_t size, int left, int right);
+int hoare_partition(int *array, size_t size, int left, int right);
+
+
 
 
 #endif
