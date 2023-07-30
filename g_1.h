b96389d794c9d884554b811b73d1c6f12d1aa7a3
Orateng <1818.blackmail@gmail.com>
2023-07-30 02:30:59 +0000

    My file

--- g_1.h
+++ g_1.h
@@ -4,20 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Comparison direction macros for bitonic sort */
 #define UP 0
 #define DOWN 1
-
-/**
- * enum bool - Enumeration of Boolean values.
- * @false: Equals 0.
- * @true: Equals 1.
- */
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -33,22 +21,37 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_s(int *array, int lo, int hi, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
 void shell_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
 void cocktail_sort_list(listint_t **list);
+listint_t *create_listint(const int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
 void heap_sort(int *array, size_t size);
+void max_heapify(int *array, size_t size, size_t base, size_t root);
 void radix_sort(int *array, size_t size);
+int get_max(int *array, int size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
 void bitonic_sort(int *array, size_t size);
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow);
 void quick_sort_hoare(int *array, size_t size);
+void hoare_sort(int *array, size_t size, int left, int right);
+int hoare_partition(int *array, size_t size, int left, int right);
 
-#endif /* SORT_H */
+
+
+
+#endif
